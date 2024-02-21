987221b60c4214ceaf10ca2bcbedd355b43e8850
Maria Celeste Vidaurre <147780140+celeste0721@users.noreply.github.com>
2024-02-20 18:21:06 -0600

    Add files via upload

---
+++ C#/ApGira/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ApGira
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ApGira.Properties.Resources.placeholder;
+            this.pictureBox2.Location = new System.Drawing.Point(386, 72);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(78, 75);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ApGira.Properties.Resources.lucky_Wheel;
+            this.pictureBox1.Location = new System.Drawing.Point(226, 153);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(389, 365);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(618, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(63, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 57);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Presiona";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(618, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "RESULTADO:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 632);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ C#/ApGira/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApGira
+{
+
+public partial class Form1 : Form
+    {
+        bool laRuedaEstaMoviendo;
+        float vueltasDeRueda;
+        Timer temporizadorDeRueda;
+        LuckyCircle circuloDeLaSuerte;
+
+        public Form1()
+        {
+            InitializeComponent();
+            temporizadorDeRueda = new Timer();
+            temporizadorDeRueda.Interval = 30; // speed
+            temporizadorDeRueda.Tick += temporizadorDeRueda_Tick;
+            circuloDeLaSuerte = new LuckyCircle();
+        }
+
+        public class LuckyCircle
+        {
+            public Bitmap imagen;
+            public Bitmap imagenTemporal;
+            public float angulo;
+            public string[] valoresDeEstado;
+            public int estado = 0;
+
+            public LuckyCircle()
+            {
+                imagenTemporal = new Bitmap(Properties.Resources.lucky_Wheel);
+                imagen = new Bitmap(Properties.Resources.lucky_Wheel);
+                valoresDeEstado = new string[] { "State A Historia", "State B Ciencia", "State C Geografia", "State D", "State E", "State F Corona", "State G", "State H Entretenimiento", "State I Arte", "State J", "State K Deporte", "State L" };
+                angulo = 0.0f;
+            }
+        }
+
+        public static Bitmap RotarImagen(Image imagen, float angulo)
+        {
+            return RotarImagen(imagen, new PointF((float)imagen.Width / 2, (float)imagen.Height / 2), angulo);
+        }
+
+        public static Bitmap RotarImagen(Image imagen, PointF offset, float angulo)
+        {
+            if (imagen == null)
+                throw new ArgumentNullException("imagen");
+
+            Bitmap imagenRotada = new Bitmap(imagen.Width, imagen.Height);
+            imagenRotada.SetResolution(imagen.HorizontalResolution, imagen.VerticalResolution);
+            Graphics g = Graphics.FromImage(imagenRotada);
+            g.TranslateTransform(offset.X, offset.Y);
+            g.RotateTransform(angulo);
+            g.TranslateTransform(-offset.X, -offset.Y);
+            g.DrawImage(imagen, new PointF(0, 0));
+            return imagenRotada;
+        }
+
+        private void RotarImagen(PictureBox pb, Image img, float angulo)
+        {
+            if (img == null || pb.Image == null)
+                return;
+
+            Image imagenAnterior = pb.Image;
+            pb.Image = RotarImagen(img, angulo);
+            if (imagenAnterior != null)
+            {
+                imagenAnterior.Dispose();
+            }
+        }
+
+        private void temporizadorDeRueda_Tick(object sender, EventArgs e)
+        {
+            if (laRuedaEstaMoviendo && vueltasDeRueda > 0)
+            {
+                circuloDeLaSuerte.angulo += vueltasDeRueda / 10;
+                circuloDeLaSuerte.angulo = circuloDeLaSuerte.angulo % 360;
+                RotarImagen(pictureBox1, circuloDeLaSuerte.imagen, circuloDeLaSuerte.angulo);
+                vueltasDeRueda--;
+            }
+
+            circuloDeLaSuerte.estado = Convert.ToInt32(Math.Ceiling(circuloDeLaSuerte.angulo / 30));
+
+            if (circuloDeLaSuerte.estado == 0)
+            {
+                circuloDeLaSuerte.estado = 0;
+            }
+            else
+            {
+                circuloDeLaSuerte.estado -= 1;
+            }
+            label1.Text = circuloDeLaSuerte.valoresDeEstado[circuloDeLaSuerte.estado];
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            laRuedaEstaMoviendo = true;
+            Random rand = new Random();
+            vueltasDeRueda = rand.Next(150, 200); // número de vueltas al azar
+            temporizadorDeRueda.Start();
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
